71c3e56094cd6223ed271af03134c3851ca64e2c
silly_silvy <silvyespindola2004@gmail.com>
2024-05-19 11:17:54 -0300

    adição de um sistema teste de verificação do ultimo caracter inserido (ainda a alterar)

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function alterarValor(novoValor){
 
 function removerUltimo(){
     var display = document.getElementById("number_display");
+    var ultimoCaracter = display.textContent.toString().slice(-1);
+    if(ultimoCaracter === '\u00D7' || ultimoCaracter === '\u00F7' || ultimoCaracter === '+' || ultimoCaracter === '-'){
+        return;
+    }
     display.textContent = document.getElementById("number_display").textContent.toString().slice(0,-1);
     if(display.textContent === ""){
         display.textContent = "0";
